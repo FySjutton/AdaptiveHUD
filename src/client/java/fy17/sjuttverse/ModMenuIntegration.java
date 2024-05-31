c2912c2e6f13af757c220861a5908c80fff9773b
Fy17 <rubik.sten@gmail.com>
2024-05-31 18:04:25 +0200

    Changed folders

--- src/client/java/fy17/sjuttverse/ModMenuIntegration.java
+++ src/client/java/fy17/sjuttverse/ModMenuIntegration.java
@@ -2,7 +2,7 @@ package fy17.sjuttverse;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import fy17.sjuttverse.configscreen.ConfigScreen;
+import fy17.sjuttverse.screens.configscreen.ConfigScreen;
 
 public class ModMenuIntegration implements ModMenuApi {
     @Override
