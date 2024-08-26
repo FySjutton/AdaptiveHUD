6aa7fd784881754aac80a5fbbe011d85e2236977
FySjutton <rubik.sten@gmail.com>
2024-08-26 11:56:25 +0200

    Added TitleCase

--- src/main/java/ahud/adaptivehud/renderhud/variables/FlagParser.java
+++ src/main/java/ahud/adaptivehud/renderhud/variables/FlagParser.java
@@ -1,11 +1,14 @@
 package ahud.adaptivehud.renderhud.variables;
 
+import org.apache.commons.lang3.text.WordUtils;
+
 public class FlagParser {
     public String parseFlags(String value, String flagString) {
         for (String x : flagString.split(" *-")) {
             switch (x) {
                 case "uc" -> value = value.toUpperCase();
                 case "lc" -> value = value.toLowerCase();
+                case "tc" -> value = WordUtils.capitalizeFully(value);
                 case "nd" -> value = value.replaceAll("[-_]", " ");
                 case "sh" -> value = value.split(":")[1];
             }
