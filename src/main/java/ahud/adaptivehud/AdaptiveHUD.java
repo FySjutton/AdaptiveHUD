28da075ad7660455ab1e457bd40eb31332d84cc2
FySjutton <rubik.sten@gmail.com>
2024-09-17 10:19:23 +0200

    Fixed cooldowned elements so they actually have a cooldown

--- src/main/java/ahud/adaptivehud/AdaptiveHUD.java
+++ src/main/java/ahud/adaptivehud/AdaptiveHUD.java
@@ -51,13 +51,17 @@ public class AdaptiveHUD implements ModInitializer {
 			Text.translatable("adaptivehud.key.category").getString()
 	));
 
+	private RenderHUD hudRenderer;
+
 	@Override
 	public void onInitialize() {
 		new ConfigFiles().CheckDefaultConfigs();
 		new ConfigFiles().generateConfigArray();
 		new ConfigFiles().GenerateElementArray();
 
-		HudRenderCallback.EVENT.register((drawContext, tickCounter) -> new RenderHUD(true).renderCustomHud(drawContext));
+		hudRenderer = new RenderHUD(true);
+
+		HudRenderCallback.EVENT.register((drawContext, tickCounter) -> hudRenderer.renderCustomHud(drawContext));
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			if (reloadElementsKeyBind.wasPressed()) {
 				new ConfigFiles().GenerateElementArray();
